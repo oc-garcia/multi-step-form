407cb2c501b53afc20708b318721bc504f6420b8
oc-garcia <octaviogarcia1988@gmail.com>
2023-06-14 16:18:41 -0300

    Finished back and next menu

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,6 +3,7 @@ import MultStepForm from "../components/Form/Form";
 import styles from "./app.module.css";
 import { Iphase } from "../types/IPhase";
 import PhaseIndicator from "../components/PhaseIndicator/PhaseIndicator";
+import StepBar from "../components/StepBar/StepBar";
 
 const phaseDefault = {
   phase1: true,
@@ -15,13 +16,16 @@ function App() {
   const [phase, setPhase] = useState(phaseDefault);
   return (
     <section className={styles.mainContainer}>
-      <div className={styles.phaseContainer}>
-        <PhaseIndicator phase={phase.phase1}>1</PhaseIndicator>
-        <PhaseIndicator phase={phase.phase2}>2</PhaseIndicator>
-        <PhaseIndicator phase={phase.phase3}>3</PhaseIndicator>
-        <PhaseIndicator phase={phase.phase4}>4</PhaseIndicator>
+      <div className={styles.topContainer}>
+        <div className={styles.phaseContainer}>
+          <PhaseIndicator phase={phase.phase1}>1</PhaseIndicator>
+          <PhaseIndicator phase={phase.phase2}>2</PhaseIndicator>
+          <PhaseIndicator phase={phase.phase3}>3</PhaseIndicator>
+          <PhaseIndicator phase={phase.phase4}>4</PhaseIndicator>
+        </div>
+        <MultStepForm phase={phase} />
       </div>
-      <MultStepForm phase={phase} />
+      <StepBar phase={phase} setPhase={setPhase} />
     </section>
   );
 }
---
+++ src/components/StepBar/StepBar.tsx
@@ -0,0 +1,34 @@
+import { Iphase } from "../../types/IPhase";
+import styles from "./stepBar.module.css";
+
+type Props = {
+  phase: Iphase;
+  setPhase: React.Dispatch<React.SetStateAction<Iphase>>;
+};
+
+export default function StepBar({ phase, setPhase }: Props) {
+  const handleNext = () => {
+    if (phase.phase1) {
+      setPhase({ ...phase, phase1: false, phase2: true });
+    }
+  };
+
+  const handleBack = () => {
+    if (phase.phase2) {
+      setPhase({ ...phase, phase1: true, phase2: false });
+    }
+  };
+
+  return (
+    <div className={phase.phase1 ? styles.stepBarContainer1 : styles.stepBarContainer234}>
+      {!phase.phase1 && (
+        <p onClick={handleBack} className={styles.backBtn}>
+          Go Back
+        </p>
+      )}
+      <p onClick={handleNext} className={styles.nextBtn}>
+        Next Step
+      </p>
+    </div>
+  );
+}
